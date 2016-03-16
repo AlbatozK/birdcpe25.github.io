a6ec45e00609d3f8a6ce011cdb799facb3556bff
Saran Khotsathian <bird.mwit18@gmail.com>
2016-03-16 23:59:12 +0700

    Clean up all code and prepare for implement Resume web blog

--- js/index.js
+++
@@ -1,6 +0,0 @@
-(function(){
-    var editor = ace.edit("editor");
-    editor.setTheme("ace/theme/xcode");
-	editor.getSession().setUseWorker(false);
-    editor.getSession().setMode("ace/mode/javascript");
-}());
