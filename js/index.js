387dec139590576a414832c4f50668815a7b3c2d
Saran Khotsathian <bird.mwit18@gmail.com>
2016-03-19 13:43:26 +0700

    update contact and align portfolio add new item

--- js/index.js
+++ js/index.js
@@ -1,9 +1,27 @@
+
+/*
+// Function to load XML
+function loadXml( xmlPath ){
+  //Generate Http Request object
+  if( $(window).XMLHttpRequest ){
+    xhttp=new XMLHttpRequest();
+  }else{
+    xhttp = new ActiveXObject("Microsoft.XMLDOM");
+  }
+  // Send request
+  xhttp.open( "GET", xmlPath, false );
+  xhttp.send();
+  // Return XML item
+  return xhttp.responseXML;
+}
+*/
+
+
 $(document).ready( function (){
 
+  // Add event scroll handler
   $(window).scroll( function(){
-
     $('.fadein-scroll').each( function(i){
-
       var upper_of_object = $(this).offset().top;
       var bottom_of_object = $(this).offset().top + $(this).outerHeight();
 
@@ -12,16 +30,9 @@ $(document).ready( function (){
       var transition_delay = 800;
       if( upper_of_object < bottom_of_window )
         $(this).animate({'opacity':'1'},transition_delay);
-/*
-      if( upper_of_object > upper_of_window || bottom_of_object > upper_of_window ){
-        $(this).animate({'opacity':'1'},transition_delay);
-      }else if( bottom_of_object <= upper_of_window || upper_of_object >= bottom_of_window ){
-        $(this).animate({'opacity':'0'},transition_delay);
-      }
-*/
-
-
     });
   });
+
+  // Add dynamic construct each section
 
 } );
